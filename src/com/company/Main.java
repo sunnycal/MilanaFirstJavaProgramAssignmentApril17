00b7368df5e7a41d1a3e3e981dc97be39e3d9837
milana.campbell <milana.campbell@gmail.com>
2019-04-17 11:35:32 -0400

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            System.out.println("Hello, World!");
+            System.out.println("Today is April 17, 2019. I am alive!");
+            System.out.println("My name is not Alton, it's Milana!");
+
+        }
+    }
